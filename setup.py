ba44ed7183577307d126ac051b59f520edd71060
Dan Paulson <danpaulson@gmail.com>
2023-08-15 11:05:58 -0500

    Fix setup

--- setup.py
+++ setup.py
@@ -1,23 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import sys
-
-from setuptools import setup
-
-sys.stderr.write(
-    """\
-===============================
-Unsupported installation method
-===============================
-This project no longer supports installation with `python setup.py install`.
-Please use `python -m pip install .` instead.
-"""
-)
-sys.exit(1)
-
-# The code below will never execute, however is required to
-# display the "Used by" section on the GitHub repository.
-#
-# See: https://github.com/github/feedback/discussions/6456
-
-setup(name="django-downstream-cache")
+if __name__ == "__main__":
+    import setuptools 
+    setuptools.setup(name="django-downstream-cache")
