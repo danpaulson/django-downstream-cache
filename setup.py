5018a0ea1bffcec5937bc8d587c768093c022c9e
Dan Paulson <danpaulson@gmail.com>
2023-08-15 11:05:12 -0500

    Add basic pyproject stuff

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-if __name__ == "__main__":
-    import setuptools 
-    setuptools.setup()
+import sys
+
+from setuptools import setup
+
+sys.stderr.write(
+    """\
+===============================
+Unsupported installation method
+===============================
+This project no longer supports installation with `python setup.py install`.
+Please use `python -m pip install .` instead.
+"""
+)
+sys.exit(1)
+
+# The code below will never execute, however is required to
+# display the "Used by" section on the GitHub repository.
+#
+# See: https://github.com/github/feedback/discussions/6456
+
+setup(name="django-downstream-cache")
